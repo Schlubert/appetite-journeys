6e8161fae9535a843fd1d739d7f24009ec9641de
Kevin <schlubert@gmail.com>
2025-10-20 10:45:45 +1300

    <BrowserRouter basename="/appetite-journeys">   <App /> </BrowserRouter>  to <BrowserRouter>   <App /> </BrowserRouter>

--- index.tsx
+++ index.tsx
@@ -12,8 +12,8 @@ if (!rootElement) {
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/appetite-journeys">
-      <App />
-    </BrowserRouter>
+    <BrowserRouter>
+  <App />
+</BrowserRouter>
   </React.StrictMode>
 );
